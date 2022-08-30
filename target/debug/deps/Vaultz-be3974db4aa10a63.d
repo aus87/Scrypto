043de38a00c3343352da246cb9958b69729ae781
aus87 <molonlabe710@pm.me>
2022-08-30 08:44:56 -0500

    adding vaultz platform

--- target/debug/deps/Vaultz-be3974db4aa10a63.d
+++ target/debug/deps/Vaultz-be3974db4aa10a63.d
@@ -1,5 +1,5 @@
-c:\Users\Austin\projects\Scrypto\Vaultz\target\debug\deps\Vaultz-be3974db4aa10a63.rmeta: src\lib.rs
+c:\Users\Austin\projects\scrypto-challenges\4-portfolio-management-yield-farming\Vaultz\target\debug\deps\Vaultz-be3974db4aa10a63.rmeta: src\lib.rs
 
-c:\Users\Austin\projects\Scrypto\Vaultz\target\debug\deps\Vaultz-be3974db4aa10a63.d: src\lib.rs
+c:\Users\Austin\projects\scrypto-challenges\4-portfolio-management-yield-farming\Vaultz\target\debug\deps\Vaultz-be3974db4aa10a63.d: src\lib.rs
 
 src\lib.rs:
--- target/debug/deps/Vaultz-dc9d928dbac9a782.d
+++ target/debug/deps/Vaultz-dc9d928dbac9a782.d
@@ -1,5 +1,5 @@
-c:\Users\Austin\projects\Scrypto\Vaultz\target\debug\deps\Vaultz-dc9d928dbac9a782.rmeta: src\lib.rs
+c:\Users\Austin\projects\scrypto-challenges\4-portfolio-management-yield-farming\Vaultz\target\debug\deps\Vaultz-dc9d928dbac9a782.rmeta: src\lib.rs
 
-c:\Users\Austin\projects\Scrypto\Vaultz\target\debug\deps\Vaultz-dc9d928dbac9a782.d: src\lib.rs
+c:\Users\Austin\projects\scrypto-challenges\4-portfolio-management-yield-farming\Vaultz\target\debug\deps\Vaultz-dc9d928dbac9a782.d: src\lib.rs
 
 src\lib.rs:
